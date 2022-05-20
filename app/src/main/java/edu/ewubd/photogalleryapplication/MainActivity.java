85a0f400048d5d2370d1ffddf6244c2018ede50a
TamannaSheme <syedatsheme03@gmail.com>
2022-05-20 20:22:16 +0600

    first commit

---
+++ app/src/main/java/edu/ewubd/photogalleryapplication/MainActivity.java
@@ -0,0 +1,69 @@
+package edu.ewubd.photogalleryapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.widget.GridView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    private GridView gridView;
+    customImageAdapter customImageAdapter;
+    ArrayList<imagesArrayList> arrayList;
+
+    private String URL = "https://muthosoft.com/univ/photos/";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        gridView = findViewById(R.id.gridView);
+        arrayList = new ArrayList<>();
+
+        httpRequest();
+    }
+    @SuppressLint("StaticFieldLeak")
+    private void httpRequest(){
+        new AsyncTask<Void, Void, String>(){
+            @Override
+            protected String doInBackground(Void... param){
+                try{
+                    String data = JSONParser.getInstance().makeHttpRequest(URL, "POST");
+                    return data;
+                }catch (Exception ex){
+                    ex.printStackTrace();
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(String data) {
+                super.onPostExecute(data);
+                //System.out.println(data);
+                String[] splitByComma = data.split(",");
+                ArrayList<imagesArrayList> arrayList = new ArrayList<>();
+                for (String a : splitByComma){
+                    String[] splitByColon = a.split(":");
+
+                    imagesArrayList imagesArrayList = new imagesArrayList(splitByColon[0], splitByColon[1]);
+                    arrayList.add(imagesArrayList);
+                }
+
+                loadData(arrayList);
+            }
+        }.execute();
+    }
+    void loadData(ArrayList arrayList){
+
+        customImageAdapter = new customImageAdapter(this,arrayList);
+        gridView.setAdapter(customImageAdapter);
+        customImageAdapter.notifyDataSetChanged();
+
+    }
+}
---
+++ app/src/main/java/edu/ewubd/photogalleryapplication/customImageAdapter.java
@@ -0,0 +1,71 @@
+package edu.ewubd.photogalleryapplication;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+public class customImageAdapter extends BaseAdapter {
+
+    Context context;
+    ArrayList<imagesArrayList> arrayList;
+    String imageURL, description;
+    public customImageAdapter(Context context, ArrayList<imagesArrayList> arrayList){
+        this.context = context;
+        this.arrayList = arrayList;
+
+    }
+    @Override
+    public int getCount() {
+        return this.arrayList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return arrayList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View rowView = inflater.inflate(R.layout.datacustomview, parent, false);
+
+        ImageView coursename = rowView.findViewById(R.id.coursename);
+
+
+
+        imagesArrayList imagesArrayList = arrayList.get(position);
+
+
+        imageURL = imagesArrayList.getimageID();
+        description = imagesArrayList.getDescription();
+        Picasso.get().load("https://muthosoft.com/univ/photos/"+imagesArrayList.getimageID()).resize(800,800).into(coursename);
+
+        rowView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, imageDescription.class);
+                intent.putExtra("imageID", imagesArrayList.getimageID());
+                intent.putExtra("imageDes", imagesArrayList.getDescription());
+                context.startActivity(intent);
+            }
+        });
+
+        return rowView;
+    }
+}
